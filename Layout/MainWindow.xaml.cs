7147d325ad0f6e592ce476e0922d0c6320a1acbf
Liu Hey <769891117@qq.com>
2024-04-29 21:55:32 +0800

    添加项目文件。

---
+++ Layout/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Layout
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnInput_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            if (button != null)
+            {
+                txtInput.Text += button.Content.ToString();
+            }
+        }
+
+        private void BtnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtInput.Text = "";
+        }
+
+        private void BtnOperator_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            if (button != null)
+            {
+                txtInput.Text += button.Content.ToString();
+            }
+            
+        }
+
+        private void BtnEquals_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string input = txtInput.Text;
+                var result = new DataTable().Compute(input, null);
+                txtInput.Text = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                txtInput.Text = "Error";
+            }
+        }
+    }
+}
